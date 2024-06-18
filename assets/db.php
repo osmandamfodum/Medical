b72dccdd6cfc12316701693945909115db049a9c
Osman alhussein <68653173+osmandamfodum@users.noreply.github.com>
2024-06-18 23:26:29 +0300

    Update db.php

--- assets/db.php
+++ assets/db.php
@@ -1,5 +1,5 @@
 <?php 
-    $con = new mysqli('localhost','root','','store');
+    $con = new mysqli('localhost','id22334366_store','Damfodum@79','id22334366_store');
 //for user informationa making available for all pages
 
     $array = $con->query("select * from users where id ='$_SESSION[userId]'");
